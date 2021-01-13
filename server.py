ee24edc40c3199a7340f2f9aa63480967d96edc4
Kayla Fang <kayfang182@gmail.com>
2021-01-13 03:32:48 -0500

    more routing

--- server.py
+++ server.py
@@ -3,8 +3,12 @@ import csv
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template("index.html")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
 
 @app.route("/login")
 def login():
