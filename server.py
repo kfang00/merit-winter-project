23c09a417a1f0b872c39fed02d98af6d4b144171
ChoiYingY <choiying.yau@stonybrook.edu>
2021-01-18 17:44:27 -0500

    added a showPw function to both login & sign up, trying to edit server for login function right now

--- server.py
+++ server.py
@@ -18,21 +18,50 @@ def submit_email():
         userdata = dict(request.form)
         email = userdata["email"]
         if(len(email) < 1):
-            return render_template("home.html", status='Please resubmit with valid information.')
+            return render_template("home.html", status='* Please resubmit with valid information. *')
         else:
             with open('data/subscriberList.csv', mode='a', newline='') as file:
                 data = csv.writer(file)
                 data.writerow([email])
-            return render_template("home.html")
+            return render_template("home.html", status='\\ Thank you for subscribing us! /')
 
 @app.route("/login")
-def login():
+def login_page():
     return render_template("login.html")
+
+# Read userInfo
+@app.route('/login', methods=["POST"])
+def login():
+    userdata = dict(request.form)
+    email = userdata["email"]
+    password = userdata["password"]
+    if(len(email) < 1 or len(password) < 1):
+        return render_template("login.html", status="Invalid email or password.")
+    with open('data/userInfo.csv') as file:
+        data = csv.reader(file, delimiter=',')
+        first_line = True
+        users = []
+        for row in data:
+            if not first_line:
+                if(row[3].strip() == email.strip() and row[5].strip() == email.strip()):
+                    users.append({
+                    "fname": row[0],
+                    "email": row[1],
+                    "password": row[2]
+                    })
+            else:
+                first_line = False
+    if(len(users) == 0):
+        status = "Account does not exist. Please sign up first!"
+    else:
+        status = "Login successful!"
+    return render_template("login.html", status=status, users=users)
 
 @app.route("/signUp")
 def signUp():
     return render_template("signUp.html")
 
+# Add new user's sign up information into userInfo
 @app.route("/signUp", methods=["GET", "POST"])
 def save_user_data():
     if request.method == "GET":
@@ -51,7 +80,7 @@ def save_user_data():
         elif(password != confirmPw):
             return render_template("signup.html", status='* The entered password do not match. Please resubmit again.')
         else:
-            with open('data/signUpData.csv', mode='a', newline='') as file:
+            with open('data/userInfo.csv', mode='a', newline='') as file:
                 data = csv.writer(file)
                 data.writerow([fname, lname, phoneNum, email, birthday, password, confirmPw])
             return render_template("signup.html", status='Thank you for signing up!')
