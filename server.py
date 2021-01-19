05c1b7a4911dec1b8d2f730ecd940b1e8d93a6cd
Kayla Fang <kayfang182@gmail.com>
2021-01-19 18:19:08 -0500

    cart functions

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import csv
+import sys
+from ast import literal_eval
 app = Flask(__name__)
+app.secret_key = "very secret key"
 
 @app.route("/")
 def index():
@@ -43,11 +46,11 @@ def login():
         users = []
         for row in data:
             if not first_line:
-                if(row[3].strip() == email.strip() and row[5].strip() == email.strip()):
+                if(row[3].strip() == email.strip() and row[5].strip() == password.strip()):
                     users.append({
                     "fname": row[0],
-                    "email": row[1],
-                    "password": row[2]
+                    "email": row[3],
+                    "password": row[5]
                     })
             else:
                 first_line = False
@@ -55,6 +58,7 @@ def login():
         status = "Account does not exist. Please sign up first!"
     else:
         status = "Login successful!"
+        session['current_user'] = users
     return render_template("login.html", status=status, users=users)
 
 @app.route("/signUp")
@@ -75,6 +79,7 @@ def save_user_data():
         birthday = userdata["bday"]
         password = userdata["password"]
         confirmPw = userdata["confirm-password"]
+        cart = []
         if(len(fname) < 1 or len(lname) < 1 or len(phoneNum) < 1 or len(email) < 1 or len(birthday) < 1 or len(password) < 1 or len(confirmPw) < 1):
             return render_template("signup.html", status='Please resubmit with valid information.')
         elif(password != confirmPw):
@@ -82,12 +87,25 @@ def save_user_data():
         else:
             with open('data/userInfo.csv', mode='a', newline='') as file:
                 data = csv.writer(file)
-                data.writerow([fname, lname, phoneNum, email, birthday, password, confirmPw])
+                data.writerow([fname, lname, phoneNum, email, birthday, password, confirmPw, cart])
             return render_template("signup.html", status='Thank you for signing up!')
 
-@app.route("/cart")
-def cart():
-    return render_template("cart.html")
+@app.route("/cart", methods=["GET", "POST"])
+def getCart():
+    user = session.get('current_user')
+    with open('data/userInfo.csv') as file:
+        data = csv.reader(file, delimiter = ',')
+        first_line = True
+        cart = []
+        for row in data:
+            if not first_line:
+                if len(user) > 0:
+                    if(row[3].strip() == user[0].get('email').strip()):
+                        cart = row[7]
+            else:
+                first_line = False
+    print(cart, flush=True)
+    return render_template("cart.html", status = "Success", cart = literal_eval(cart))
 
 @app.route("/about")
 def about():
@@ -96,6 +114,43 @@ def about():
 @app.route("/men")
 def men():
     return render_template("men.html")
+
+@app.route("/men", methods=["POST"])
+@app.route("/women", methods=["POST"])
+@app.route("/kids", methods=["POST"])
+@app.route("/accessory", methods=["POST"])
+def addToCart():
+    user = session.get('current_user')
+    item = request.form.to_dict()
+    name = item["name"]
+    image = item["image"]
+    price = item["price"]
+    file1 = open('data/userInfo.csv', 'rt')
+    r = csv.reader(file1, delimiter=',')
+    first_line = True
+    data = []
+    tem = []
+    for row in r:
+        if not first_line:
+            if len(user) > 0:
+                if(row[3].strip() == user[0].get('email').strip()):
+                    temp = literal_eval(row[7])
+                    #temp = row[7][1:-1].replace("'", '').strip().split(',')
+                    temp.append([image, name, price])
+                    hold = row[0:7]
+                    hold.append(temp)
+                    data.append(hold)
+                else:
+                    data.append(row)
+        else:
+            first_line = False
+            data.append(row)
+    file1.close()
+    file2 = open('data/userInfo.csv', 'w')
+    writer = csv.writer(file2,delimiter=',')
+    writer.writerows(data)
+    file2.close()
+    return redirect(url_for('getCart'))
 
 @app.route("/women")
 def women():
